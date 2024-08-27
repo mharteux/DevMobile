e1a611808caa421b2e5d27535a4714acaa8c8b21
Matheus Reynaud Soares <matheuxreynaud@gmail.com>
2024-08-27 14:48:04 -0300

    aula01

--- Aula01/App.js
+++ Aula01/App.js
@@ -2,14 +2,16 @@ import { View, Text } from 'react-native'
 import React from 'react'
 //import Exerc01 from './src/components/Exerc01'
 //import Exerc02 from './src/components/Exerc02';
-import Exerc03 from './src/components/Exerc03';
+//import Exerc03 from './src/components/Exerc03';
+import Exerc04 from './src/components/Exerc04';
 
 const App = () => {
   return (
     <View>
       {/*<Exerc01 nome='Matheus' />*/}
       {/*<Exerc02 />*/}
-      <Exerc03 />
+      {/*<Exerc03 />*/}
+      <Exerc04 />
     </View>
   );
 }
