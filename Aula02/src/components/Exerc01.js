36d9aca35795797a0d3ede3fdbad777ffedfd742
Matheus Reynaud Soares <matheuxreynaud@gmail.com>
2024-08-29 20:26:38 -0300

    aula02

---
+++ Aula02/src/components/Exerc01.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Exerc01 = () => {
+    return (
+        <View>
+            <Text>Exerc01</Text>
+        </View>
+    )
+}
+
+export default Exerc01
