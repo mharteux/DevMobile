74a798caa4e52bfac009fa81309fdc76e2da0fc1
Matheus Reynaud Soares <matheuxreynaud@gmail.com>
2024-08-30 11:10:04 -0300

    aula02

--- Aula02/App.js
+++ Aula02/App.js
@@ -4,6 +4,7 @@ import Exerc01 from './src/components/Exerc01';
 import Exerc02 from './src/components/Exerc02';
 import Exerc03 from './src/components/Exerc03';
 import Exerc04 from './src/components/Exerc04';
+import Exerc05Challenge from './src/components/Exerc05Challenge';
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       {/* <Exerc01 /> */}
       {/* <Exerc02 /> */}
       {/* <Exerc03 /> */}
-      <Exerc04 />
+      {/* <Exerc04 /> */}
+      <Exerc05Challenge />
       <StatusBar style="auto" />
     </View>
   );
--- Aula02/src/components/Exerc05Challenge.js
+++ Aula02/src/components/Exerc05Challenge.js
@@ -12,15 +12,82 @@ e um botão para adicionar a tarefa à lista.
 • Lista de Tarefas: Área onde as tarefas adicionadas serão listadas.
 */
 
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text, TextInput, Button, ScrollView } from 'react-native';
+import React, { useState } from 'react'
 
 const Exerc05Challenge = () => {
+
+    const [task, setTask] = useState('');
+    const [taskList, setTaskList] = useState([]);
+
+    const addTask = () => {
+        if (task.trim().length > 0) {
+            setTaskList([...taskList, task]);
+            setTask('');
+        }
+    };
+
     return (
-        <View>
-            <Text>Exerc05Challenge</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Your Task List</Text>
+            </View>
+
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Add a Task"
+                    value={task}
+                    onChangeText={setTask} />
+                <Button title="Add" onPress={addTask} />
+            </View>
+
+            <ScrollView style={styles.taskList}>
+                {taskList.map((item, index) => (
+                    <View key={index} style={styles.task}>
+                        <Text>{item}</Text>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 50
+    },
+    header: {
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'powderblue'
+    },
+    headerText: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        padding: 80,
+        paddingBottom: 110
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        padding: 5
+    },
+    input: {
+        flex: 1,
+        borderBottomWidth: 1,
+        marginRight: 10,
+    },
+    taskList: {
+        flex: 1
+    },
+    task: {
+        backgroundColor: 'powderblue',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 5
+    },
+});
 
 export default Exerc05Challenge
